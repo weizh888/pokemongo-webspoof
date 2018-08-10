1be703be1859ad4144de50a487542201d82b635e
gaudryc <chris.m.gaudry@gmail.com>
2018-08-10 22:59:00 +0200

    Change Key

--- src/models/settings.js
+++ src/models/settings.js
@@ -4,7 +4,7 @@ export default {
   updateXcodeLocation: observable.box(false),
   addJitterToMoves: observable.box(true),
   stationaryUpdates: observable.box(true),
-  speedLimit: observable.box(4), // ~40-25 km/h
+  speedLimit: observable.box(4), // <?? km/h
   zoom: observable.box(17), // map zoom
-  googleAPIKey: observable.box('AIzaSyCl48fRaPu1dz3XpgcOJmtIIj9oZmHAgx8') // Google API Key (private key)
+  googleAPIKey: observable.box('') // Google API Key (private key)
 }
